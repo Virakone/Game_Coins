0e0a463245a3ed5b52ee332717500bc5ceafdcee
Virakone <virakonekeophommavong@Virakones-MacBook-Pro.local>
2015-08-20 15:04:08 +0700

    BitmapFont

--- core/src/applpb/virakone/laos/MyGdxGame.java
+++ core/src/applpb/virakone/laos/MyGdxGame.java
@@ -2,26 +2,54 @@ package applpb.virakone.laos;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture wallpaperTexture;
+	private OrthographicCamera objOrthographicCamera;
+	private BitmapFont nameBitmapFont;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		objOrthographicCamera = new OrthographicCamera();
+		objOrthographicCamera.setToOrtho(false, 1200, 800);
+
+		//Setup Wallpaper
+		wallpaperTexture = new Texture("wall.png");
+
+		//Setup BitmapFont
+		nameBitmapFont = new BitmapFont();
+		nameBitmapFont.setColor(Color.RED);
+		nameBitmapFont.setScale(4);
+
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		//Setup Screen
+		objOrthographicCamera.update();
+		batch.setProjectionMatrix(objOrthographicCamera.combined);
+
+		// เอาไว้วาด object
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		//Draw Wallpaper
+		batch.draw(wallpaperTexture, 0 , 0);
+
+		//Drawable BitmapFont
+		nameBitmapFont.draw(batch, "Coins Mario Game", 40, 760);
+
 		batch.end();
 	}
 }
