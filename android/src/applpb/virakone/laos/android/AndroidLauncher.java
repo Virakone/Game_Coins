7683809afb2228a395684b300752e0c03761f7f6
Virakone <virakonekeophommavong@Virakones-MacBook-Pro.local>
2015-08-20 13:50:26 +0700

    Start App

---
+++ android/src/applpb/virakone/laos/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package applpb.virakone.laos.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import applpb.virakone.laos.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
