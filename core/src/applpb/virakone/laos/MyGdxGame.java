fb695bc6fbe6c15a6ad8beee9613ecae64d0580f
Virakone <virakonekeophommavong@Virakones-MacBook-Pro.local>
2015-08-20 16:49:38 +0700

    Finish4

--- core/src/applpb/virakone/laos/MyGdxGame.java
+++ core/src/applpb/virakone/laos/MyGdxGame.java
@@ -2,20 +2,26 @@ package applpb.virakone.laos;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture wallpaperTexture, cloudTexture;
+	Texture wallpaperTexture, cloudTexture, rilakTexture;
 	private OrthographicCamera objOrthographicCamera;
 	private BitmapFont nameBitmapFont;
 	private int xCloudAnInt, yCloudAnInt = 700;
 	private boolean cloudABoolean = true;
+	private Rectangle rilakRectangle;
+	private Vector3 objVector3;
+	private Sound rilakSound;
 
 	@Override
 	public void create () {
@@ -34,6 +40,19 @@ public class MyGdxGame extends ApplicationAdapter {
 
 		//Setup Cloud
 		cloudTexture = new Texture("cloud.png");
+
+		//Setup Rilak
+		rilakTexture = new Texture("114790096.png");
+
+		//Setup RilakRectangle
+		rilakRectangle = new Rectangle();
+		rilakRectangle.x = 494;
+		rilakRectangle.y = 80;
+		rilakRectangle.width = 212;
+		rilakRectangle.height = 245;
+
+		//Setup Rilakkuma Sound
+		rilakSound = Gdx.audio.newSound(Gdx.files.internal("pig.wav"));
 
 	}
 
@@ -58,6 +77,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		//Drawable BitmapFont
 		nameBitmapFont.draw(batch, "Coins Mario Game", 40, 760);
 
+		//Draw Rilakkuma
+		batch.draw(rilakTexture, rilakRectangle.x, rilakRectangle.y);
+
 
 
 		batch.end();
@@ -65,6 +87,36 @@ public class MyGdxGame extends ApplicationAdapter {
 		//Move Cloud
 		moveCloud();
 
+		//Active When Touch Screen
+		activeTouchScreen();
+
+	}
+
+	private void activeTouchScreen() {
+
+		if (Gdx.input.isTouched()) {
+
+			//Sound Effect Rilakkuma
+			rilakSound.play();
+
+			objVector3 = new Vector3();
+			objVector3.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+
+			if (objVector3.x < 600) {
+				if (rilakRectangle.x < 0) {
+					rilakRectangle.x = 0;
+				} else {
+					rilakRectangle.x -= 10;
+				}
+			} else {
+				if (rilakRectangle.x > 1000) {
+					rilakRectangle.x = 1000;
+				} else {
+					rilakRectangle.x += 10;
+				}
+			}
+
+		} //If
 
 	}
 
